f37c84c0fb86f0c05c939c76e898baadd859f593
赵伟 <zhaowei_ha@126.com>
2017-07-07 11:31:33 +0800

    waiqin365 代码自动生成

--- src/main/java/com/myframework/autocode/config/Config.java
+++ src/main/java/com/myframework/autocode/config/Config.java
@@ -16,26 +16,37 @@ public class Config {
      * 数据库配置
      * GENERATE_TYPE 为2时用到
      */
-    public final static DbTypeEnum DB_TYPE = DbTypeEnum.MYSQL;  //1:mysql  2:oracle
+    /*public final static DbTypeEnum DB_TYPE = DbTypeEnum.MYSQL;  //1:mysql  2:oracle
     public static final String DB_URL = "jdbc:mysql://sunbiyun.com:3306/autocode?useUnicode=true&characterEncoding=utf8";
     public static final String DB_NAME = "com.mysql.jdbc.Driver";
     public static final String DB_USER = "zhaowei";
     public static final String DB_PASSWORD = "zw900730";
-    public static final String DB_TABLES = "tmpl_tbl";
-
-    /*public static final String DB_URL = "jdbc:postgresql://172.31.3.206:5432/u8958085892090750662?useUnicode=true&characterEncoding=utf8";
-    public static final String DB_NAME = "org.postgresql.Driver";
-    public static final String DB_USER = "iorder_master";
-    public static final String DB_PASSWORD = "FHuma025";
     public static final String DB_TABLES = "tmpl_tbl";*/
-
     /**
      * 生产文件配置
      */
+    /*public final static String DAO_TYPE = "2";  //1:原生javaDao  2:接口型dao，mybatis实例化
+    public final static String AUTOCODE_PATH = "../template";
+    public final static String OUTPUT_PATH = "/out/AutoCodeGenerate/".replaceAll("/",File.separator);
+    public final static String OUTPUT_PACKAGE = "com.zw";
+    public final static String DB_PREFIX = "pt_";*/
+
+
+    /**
+     * for waiqin365
+     */
+    public final static DbTypeEnum DB_TYPE = DbTypeEnum.POSTGRESQL;
+    public static final String DB_URL = "jdbc:postgresql://172.31.3.206:5432/u8958085892090750662?useUnicode=true&characterEncoding=utf8";
+    public static final String DB_NAME = "org.postgresql.Driver";
+    public static final String DB_USER = "iorder_master";
+    public static final String DB_PASSWORD = "FHuma025";
+    public static final String DB_TABLES = "stdom_order";
+
     public final static String DAO_TYPE = "2";  //1:原生javaDao  2:接口型dao，mybatis实例化
     public final static String AUTOCODE_PATH = "../template";
     public final static String OUTPUT_PATH = "/out/AutoCodeGenerate/".replaceAll("/",File.separator);
     public final static String OUTPUT_PACKAGE = "com.zw";
-    public final static String DB_PREFIX = "pt_";
+    public final static String DB_PREFIX = "";
+
 
 }
---
+++ src/main/java/com/myframework/autocode/generator/GeneratorWaiqin365Code.java
@@ -0,0 +1,111 @@
+package com.myframework.autocode.generator;
+
+import com.myframework.autocode.config.Config;
+import com.myframework.autocode.entity.ClassInfo;
+import com.myframework.autocode.entity.ColumnInfo;
+import com.myframework.autocode.entity.PropertyInfo;
+import com.myframework.autocode.entity.TableInfo;
+import com.myframework.autocode.util.CodeGeneratorUtils;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zw on 2017/7/7.
+ */
+public class GeneratorWaiqin365Code {
+    public static void main(String[] args) throws IOException, TemplateException {
+        List<TableInfo> tableInfoList = CodeGeneratorUtils.parseTable();
+        List<ClassInfo> classInfoList = new ArrayList<ClassInfo>();
+        for (TableInfo tableInfo : tableInfoList)
+        {
+            ClassInfo classInfo = new ClassInfo(tableInfo);
+            classInfoList.add(classInfo);
+        }
+
+        System.out.println(tableInfoList);
+        System.out.println(classInfoList);
+
+        String basePath = System.getProperty("user.dir") + Config.OUTPUT_PATH;
+        // 清空basePath下的文件
+        clearBasePathFiles(basePath);
+
+        for (int i = 0; i < tableInfoList.size(); i++) {
+            TableInfo tableInfo = tableInfoList.get(i);
+            ClassInfo classInfo = classInfoList.get(i);
+
+            // 模板内使用的参数Map
+            Map<String, Object> dataMap = new HashMap<String, Object>();
+            dataMap.put("tableName", tableInfo.getTableName());
+            dataMap.put("tableDbName", tableInfo.getTableDbName());
+            dataMap.put("tableDbNameLc", tableInfo.getTableDbName().toLowerCase());
+            dataMap.put("prefixTableDbName", Config.DB_PREFIX + tableInfo.getTableDbName());
+            //
+            dataMap.put("classPackage", Config.OUTPUT_PACKAGE + "."
+                    + tableInfo.getModuleName());
+            dataMap.put("className", classInfo.getClassName());
+            dataMap.put("classNameWithoutEntity", classInfo.getClassNameWithoutEntity());
+            dataMap.put("classObjectName", classInfo.getClassObjectName());
+            dataMap.put("iDaoName", classInfo.getIDaoName());
+            dataMap.put("daoName", classInfo.getDaoName());
+
+            List<Map<String, Object>> pros = new ArrayList<Map<String, Object>>();
+            List<ColumnInfo> columnList = tableInfo.getColumns();
+            List<PropertyInfo> propertyList = classInfo.getProperties();
+            for (int j = 0; j < columnList.size(); j++) {
+                ColumnInfo columnInfo = columnList.get(j);
+                PropertyInfo propertyInfo = propertyList.get(j);
+                Map<String, Object> proMap = new HashMap<String, Object>();
+                proMap.put("proDescription", columnInfo.getDescription());
+                proMap.put("proColumnName", columnInfo.getName());
+                proMap.put("proColumnNameLc", columnInfo.getName().toLowerCase());
+                proMap.put("proDbColumnType", columnInfo.getDbColumnType(Config.DB_TYPE.MYSQL));
+                proMap.put("proDbColumnOracleType", columnInfo.getDbColumnType(Config.DB_TYPE.ORACLE));
+                proMap.put("proNotNull", columnInfo.isNotNull());
+                proMap.put("proType", propertyInfo.getPropertyType());
+                proMap.put("proName", propertyInfo.getPropertyName());
+                proMap.put("proMethodName", propertyInfo.getMethodName());
+                pros.add(proMap);
+            }
+            dataMap.put("properties", pros);
+
+            // Pojo生成
+            File pathFile = new File(basePath + tableInfo.getModuleName() +"/pojo/" + classInfo.getClassName() + ".java");
+            CodeGeneratorUtils.outFile(pathFile,"waiqin365/PojoTemplate",dataMap);
+            // mapper生成
+            pathFile = new File(basePath + tableInfo.getModuleName() +"/mapper/" + classInfo.getClassName() + ".java");
+            CodeGeneratorUtils.outFile(pathFile,"waiqin365/MapperTemplate",dataMap);
+            // mapping生产
+            pathFile = new File(basePath + tableInfo.getModuleName() +"/mapping/" + classInfo.getClassName() + ".java");
+            CodeGeneratorUtils.outFile(pathFile,"waiqin365/MappingTemplate",dataMap);
+        }
+    }
+
+
+    private static void clearBasePathFiles(String basePath)
+    {
+        File dir = new File(basePath);
+        dir.mkdirs();
+        clearBasePathFiles(dir);
+    }
+
+    private static void clearBasePathFiles(File file)
+    {
+        if (file.isDirectory())
+        {
+            for (File child : file.listFiles())
+            {
+                clearBasePathFiles(child);
+            }
+        }
+        else
+        {
+            file.delete();
+        }
+    }
+}
